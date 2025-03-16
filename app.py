1e44f6dd66137f12b2c4ac9875757e9e60efa03a
Alekstrof <aleksejtrof92@gmail.com>
2025-03-16 16:46:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ if st.button('Вычислить скользящее среднее'):
 if st.button('Рассчитать среднюю температуру и стандартное отклонение'):
     st.session_state.stats = calculate_seasonal_stats(st.session_state.data)
     st.session_state.data=st.session_state.data.merge(st.session_state.stats, on=['city', 'season']) #это делаем для дальнейшего поиска аномалий
-    st.dataframe(st.session_state.data)
+    st.dataframe(st.session_state.stats)
 
 # Кнопка для выявления аномалий
 if st.button('Выявить аномалии'):
